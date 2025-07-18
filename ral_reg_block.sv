4d57a68046795b6febabf31e1383e758939627c1
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-07-18 11:48:06 +0530

    Update ral_reg_block.sv

--- ral_reg_block.sv
+++ ral_reg_block.sv
@@ -1,7 +1,6 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
-
 class slv_reg1 extends uvm_reg;
   `uvm_object_utils(slv_reg1)
   rand uvm_reg_field reg1;
@@ -42,6 +41,20 @@ class slv_reg3 extends uvm_reg;
   function void build();
     reg3 = uvm_reg_field::type_id::create("reg3");
     reg3.configure(this, 32, 0, "RW", 0,  32'h5A5A_5555, 1, 1, 1);
+  endfunction
+endclass
+
+class slv_reg4 extends uvm_reg;
+  `uvm_object_utils(slv_reg4)
+  rand uvm_reg_field reg4;
+
+  function new(string name="slv_reg4");
+    super.new(name, 32, UVM_NO_COVERAGE);
+  endfunction
+
+  function void build();
+    reg4 = uvm_reg_field::type_id::create("reg4");
+    reg4.configure(this, 32, 0, "RW", 0, 32'h0000_FFFF, 1, 1, 1);
   endfunction
 endclass
 
