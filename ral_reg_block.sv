4755ce3d2153e62ae1d11f4003d4edb0a44b6ce7
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-07-18 11:47:41 +0530

    Update ral_reg_block.sv

--- ral_reg_block.sv
+++ ral_reg_block.sv
@@ -31,6 +31,20 @@ class slv_reg2 extends uvm_reg;
   endfunction
 endclass
 
+class slv_reg3 extends uvm_reg;
+  `uvm_object_utils(slv_reg3)
+  rand uvm_reg_field reg3;
+
+  function new(string name="slv_reg3");
+    super.new(name, 32, UVM_NO_COVERAGE);
+  endfunction
+
+  function void build();
+    reg3 = uvm_reg_field::type_id::create("reg3");
+    reg3.configure(this, 32, 0, "RW", 0,  32'h5A5A_5555, 1, 1, 1);
+  endfunction
+endclass
+
 class apb_reg_block extends uvm_reg_block;
   `uvm_object_utils(apb_reg_block)
  
