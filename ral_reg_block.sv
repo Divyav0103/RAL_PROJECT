352c73deb837bac5e480c0759ced18d8f4d0fcfc
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-07-18 11:49:44 +0530

    Update ral_reg_block.sv

--- ral_reg_block.sv
+++ ral_reg_block.sv
@@ -1,6 +1,21 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
+class slv_ctrl extends uvm_reg;
+  `uvm_object_utils(slv_ctrl)
+  rand uvm_reg_field ctrl;
+
+  function new(string name="slv_ctrl");
+    super.new(name, 4, UVM_NO_COVERAGE);
+  endfunction
+
+  function void build();
+    ctrl= uvm_reg_field::type_id::create("ctrl");
+    ctrl.configure(this, 4, 0, "RW", 0, 0, 1, 1, 1);
+ 
+  endfunction
+endclass
+
 class slv_reg1 extends uvm_reg;
   `uvm_object_utils(slv_reg1)
   rand uvm_reg_field reg1;
