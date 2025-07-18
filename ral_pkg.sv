05d8edb45a88c23b5c4a9a1dad19f12c4fd5872b
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-07-18 12:10:22 +0530

    Update ral_pkg.sv

--- ral_pkg.sv
+++ ral_pkg.sv
@@ -1,14 +1,6 @@
 `include "ral_reg_block.sv"
 
-`include "ral_sequence.sv"
-
 `include "ral_adapter.sv"
-
-`include "ral_reg1.sv"
-`include "ral_reg2.sv"
-`include "ral_reg3.sv"
-`include "ral_reg4.sv"
-//`include "ral_generator.sv"
 
 `include "ral_sequence.sv"
 
@@ -23,8 +15,7 @@
 `include "ral_agent.sv"
 
 `include "ral_env.sv"
-`include "ral_ctrl.sv"
-// `include "ral_if.sv"
+
 `include "ral_test.sv"
 
  
