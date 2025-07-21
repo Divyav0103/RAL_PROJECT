c0f568c7f4acededc4b838d31696335345cc802d
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-07-21 10:14:28 +0530

    Update ral_top.sv

--- ral_top.sv
+++ ral_top.sv
@@ -2,30 +2,38 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;  
 `include "ral_pkg.sv"
- `include "ral_if.sv"
+`include "ral_if.sv"
 `include "design.v"
 import ral_pkg::*;
 
 module tb;
- bit CLK,PRESETn=0; 
-  apb_slave  dut(CLK,PRESETn,vif.PSEL,vif.PENABLE,vif.PWRITE,vif.PADDR,vif.PWDATA,vif.PRDATA);
-  ral_if vif(CLK,PRESETn);
-  initial begin
-    CLK=0;
-    PRESETn=1;
-#10 PRESETn=0;
-  end
+ bit CLK, PRESETn = 0;
+ 
+ initial begin
+  CLK = 0;
+  PRESETn = 1;
+  #10 PRESETn = 0;
+ end
+
+ ral_if vif(CLK,PRESETn);
+ top dut(.pclk(vif.CLK),
+         .presetn(vif.PRESETn),
+         .psel(vif.PSEL),
+         .penable(vif.PENABLE),
+         .pwrite(vif.PWRITE),
+         .paddr(vif.PADDR),
+         .pwdata(vif.PWDATA),
+         .prdata(vif.PRDATA));
+ 
+ always #5 CLK = ~CLK;
+ 
+ initial begin
+  uvm_config_db#(virtual ral_if)::set(null,"*","vif",vif);
+  run_test("apb_test");
+ end
   
-  always #5 CLK =~CLK;
-  
-  initial begin
-    uvm_config_db#(virtual ral_if)::set(null,"*","vif",vif);
-    
-    run_test("apb_test");
-  end
-  
-  initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars;
+ initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars;
   end
 endmodule
