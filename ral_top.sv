2ee1252c87ed5cdc8b36ceb466d79cab0059e816
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-08-05 09:42:49 +0530

    update

--- ral_top.sv
+++ ral_top.sv
@@ -29,7 +29,7 @@ module tb;
  
  initial begin
   uvm_config_db#(virtual ral_if)::set(null,"*","vif",vif);
-  run_test("apb_frontdoor_test");
+  run_test("apb_backdoor_test");
  end
   
  initial begin
