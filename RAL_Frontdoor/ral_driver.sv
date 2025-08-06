00d0b66287d3739322f207bbd8a89dc8444d1202
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-08-06 19:56:03 +0530

    Update ral_driver.sv

--- RAL_Frontdoor/ral_driver.sv
+++ RAL_Frontdoor/ral_driver.sv
@@ -16,6 +16,12 @@ class apb_driver extends uvm_driver#(apb_transaction);
 
   virtual task run_phase(uvm_phase phase);
     tr = apb_transaction::type_id::create("tr");
+    vif.drv_cb.psel    <= 1'b0;
+    vif.drv_cb.penable <= 1'b0;
+    vif.drv_cb.pwrite  <= 1'b0;
+    vif.drv_cb.paddr   <= 32'h0;
+    vif.drv_cb.pwdata  <= 32'h0;
+    
     forever begin
       seq_item_port.get_next_item(tr);
       drive(tr);
@@ -25,35 +31,35 @@ class apb_driver extends uvm_driver#(apb_transaction);
 
   task drive(apb_transaction tr);
     if(tr.PWRITE == 1'b1) begin
-    @(posedge vif.PCLK);
-    vif.PRESETn <= 1'b1;
-    vif.PSEL <= 1'b1;
-    vif.PWDATA <= tr.PWDATA;
-    vif.PWRITE <= 1'b1;
-    vif.PADDR <= tr.PADDR;
-    
-    repeat(2) @(posedge vif.PCLK);
-    vif.PENABLE <= 1'b1;
+      @(vif.drv_cb);
+      vif.PRESETn <= 1'b1;
+      vif.drv_cb.PSEL <= 1'b1;
+      vif.drv_cb.PWDATA <= tr.PWDATA;
+      vif.drv_cb.PWRITE <= 1'b1;
+      vif.drv_cb.PADDR <= tr.PADDR;
+      
+      repeat(2) @(vif.drv_cb);
+      vif.drv_cb.PENABLE <= 1'b1;
     `uvm_info("DRV", $sformatf("Data Write -> Wdata : %0h",vif.PWDATA),UVM_NONE);
-    
-    @(posedge vif.PCLK);
-      vif.PSEL = 1'b0;
-      vif.PENABLE <= 1'b0;
+      
+      @(vif.drv_cb);
+      vif.drv_cb.PSEL = 1'b0;
+      vif.drv_cb.PENABLE <= 1'b0;
    end else begin
       
-      @(posedge vif.PCLK);
-         vif.PSEL    <= 1'b1;
-         vif.PADDR   <= tr.PADDR;
-         vif.PWRITE  <= 1'b0;
-        
-         repeat(2)@(posedge vif.PCLK);
-         vif.PENABLE <= 1'b1;
-         `uvm_info("DRV", $sformatf("Data READ -> read data : %0h",vif.PRDATA),UVM_NONE);
-      @(posedge vif.PCLK);
-         vif.PSEL <= 1'b0;
-         vif.PENABLE <=1'b0;
-         tr.PRDATA = vif.PRDATA;
+     @(vif.drv_cb);
+         vif.drv_cb.PSEL    <= 1'b1;
+         vif.drv_cb.PADDR   <= tr.PADDR;
+         vif.drv_cb.PWRITE  <= 1'b0;
+     
+     repeat(2)@(vif.drv_cb);
+     vif.drv_cb.PENABLE <= 1'b1;
+     `uvm_info("DRV", $sformatf("Data READ -> read data : %0h",vif.PRDATA),UVM_NONE);
+     @(vif.drv_cb);
+         vif.drv_cb.PSEL <= 1'b0;
+         vif.drv_cb.PENABLE <=1'b0;
+         tr.PRDATA = vif.drv_cb.PRDATA;
       end
-  `uvm_info("drv",$sformatf("-----------------------------------DRIVER DONE--------------------"), UVM_LOW);
-   endtask
+    `uvm_info("drv",$sformatf("-----------------------------------DRIVER DONE--------------------"), UVM_LOW);
+  endtask
 endclass
