b6e1a2f2f4d5cf22bca7831d64fe09749b1d8372
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-08-06 15:14:53 +0530

    Create ral_top.sv

---
+++ RAL_Frontdoor/ral_top.sv
@@ -0,0 +1,36 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;  
+`include "ral_pkg.sv"
+`include "design.v"
+
+module tb;
+ bit PCLK, PRESETn;
+ 
+ initial begin
+  PCLK = 0;
+  PRESETn = 0;
+  #10 PRESETn = 1;
+ end
+
+ ral_if vif(.PCLK(PCLK),.PRESETn(PRESETn));
+ top dut(.pclk(vif.PCLK),
+         .presetn(vif.PRESETn),
+         .psel(vif.PSEL),
+         .penable(vif.PENABLE),
+         .pwrite(vif.PWRITE),
+         .paddr(vif.PADDR),
+         .pwdata(vif.PWDATA),
+         .prdata(vif.PRDATA));
+ 
+ always #5 PCLK = ~PCLK;
+ 
+ initial begin
+  uvm_config_db#(virtual ral_if)::set(null,"*","vif",vif);
+  run_test("reg_test");
+ end
+  
+ initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars;
+  end
+endmodule
