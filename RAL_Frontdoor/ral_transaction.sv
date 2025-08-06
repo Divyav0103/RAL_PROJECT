9ca8848f0e3a091fdef91d79a37d15347af7c56c
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-08-06 15:16:24 +0530

    Create ral_transaction.sv

---
+++ RAL_Frontdoor/ral_transaction.sv
@@ -0,0 +1,17 @@
+class apb_transaction extends uvm_sequence_item;
+  
+  rand bit PWRITE;
+  rand bit PSEL;
+  rand bit PENABLE;
+  rand bit [31:0] PADDR;
+  rand bit [31:0] PWDATA;
+       bit [31:0] PRDATA;
+  
+  `uvm_object_utils(apb_transaction)
+
+  function new(string name = "apb_transaction");
+    super.new(name);
+  endfunction
+
+  constraint c0{PADDR inside {0, 4, 8, 12, 16};}
+endclass
