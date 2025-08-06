37450cd9292f3183f09ea1d6675d3f3c42ef921e
Divyav0103 <141170378+Divyav0103@users.noreply.github.com>
2025-08-06 19:49:32 +0530

    Update ral_if.sv

--- RAL_Frontdoor/ral_if.sv
+++ RAL_Frontdoor/ral_if.sv
@@ -6,18 +6,29 @@ interface ral_if(input logic PCLK, input bit PRESETn);
   logic [31:0]  PADDR;
   logic [31:0] PWDATA;
   logic [31:0] PRDATA;
-/* 
+ 
   // Clocking block for synchronous access in driver/monitor
-  clocking cb @(posedge PCLK);
-    default input #1 output #1;
- 
-    output PSEL;
-    output PENABLE;
-    output PWRITE;
-    output PADDR;
-    output PWDATA;
- 
-    input  PRDATA;
+  clocking drv_cb @(posedge PCLK);
+   default input #0 output #0;
+   input PRESETn;
+   inout PSEL;
+   inout PENABLE;
+   inout PWRITE;
+   inout PADDR;
+   inout PWDATA;
+   input  PRDATA;
   endclocking
- */
+   
+ clocking mon_cb @(posedge PCLK);
+  default input #0 output #0;
+  input PSEL;
+  input PENABLE;
+  input PWRITE;
+  input PADDR;
+  input PWDATA;
+  input  PRDATA;
+ endclocking
+  
+ modport DRV(clocking drv_cb, input PCLK, PRESETn, PRDATA);
+ modport MON(clocking mon_cb, input PCLK, PRESETn);
 endinterface
